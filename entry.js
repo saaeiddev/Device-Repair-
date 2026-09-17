8a948e8e9374972689987e1a94cd1724c9919d7e
Amir Saeid Dehghan <108151841+saaeiddev@users.noreply.github.com>
2026-09-17 15:28:01 +0330

    Load ultra-realistic device shell upgrade

--- entry.js
+++ entry.js
@@ -1,4 +1,5 @@
 import './room-enhancement.js?v=1';
 import './room-lighting.js?v=1';
 import './device-hyperreal.js?v=1';
+import './device-ultra-real.js?v=1';
 await import('./app-realistic.js?v=2');
